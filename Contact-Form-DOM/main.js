218940c45e365ca46a010591e61715f986dab823
Orasan Lucian <lucianraul2003@gmail.com>
2023-06-02 20:59:38 +0300

    contact form with DOM

--- Contact-Form-DOM/main.js
+++ Contact-Form-DOM/main.js
@@ -1,7 +1,24 @@
-import "./style.css";
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const submitBtn = document.getElementById("buton");
+const validationMessage = document.getElementById("request");
+const userMessage = document.getElementById("your-message");
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    Final project
-  </div>
-`;
+submitBtn.addEventListener("click", onSubmitShow);
+function onSubmitShow() {
+  if (firstNameInput.value.trim() === "" || lastNameInput.value.trim() === "") {
+    firstNameInput.style.border = "2px solid";
+    firstNameInput.style.borderColor = "red";
+    lastNameInput.style.border = "2px solid";
+    lastNameInput.style.borderColor = "red";
+  } else {
+    firstNameInput.style.borderColor = "";
+    lastNameInput.style.borderColor = "";
+    validationMessage.style.backgroundColor = "greenyellow";
+    document.getElementById("request").innerHTML =
+      "Thank you for contacting us, " + lastNameInput.value;
+    console.log("First name: " + firstNameInput.value);
+    console.log("Last name: " + lastNameInput.value);
+    console.log("Your message here: " + userMessage.value);
+  }
+}
--- README.md
+++ README.md
@@ -1,29 +1,13 @@
-### Install dependencies
+Implement a Contact form component. Your form should have: 2 text inputs, 1 radio button, 1
+text area, 1 submit button and an optional image on the right, just to make the form look a little
+better.
 
-```bash
-cd Magazin electronic
-```
+● When clicking the “Submit” button, you should validate the required fields , and display a
+red border around the input if validation fails.
 
-```bash
-npm install
-```
+● If user corrects the error, you should remove the red border.
 
-### Using dev environment
-
-```bash
-npm run dev
-```
-
-Open vite dev server:
-
-CTRL+click on http://127.0.0.1:5173/
-
-### Build and deploy
-
-```bash
-npm run build
-```
-
-```bash
-npx surge dist OrasanLucian-FinalProject.surge.sh
-```
+● When the form is successfully submitted (no validation errors) you should display a
+Confirmation banner (which is hidden initially) containing the user’s first name that you
+just collected from the input fields. Also all the info from the input fields should be printed
+in the Console.
